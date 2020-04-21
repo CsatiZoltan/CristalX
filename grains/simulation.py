8d01417e321916b9d84b20d8d8bb2bbcb2c5b36e
CsatiZoltan <csati_zoltan@freemail.hu>
2020-04-21 18:16:35 +0200

    Change the domain of an image.

--- grains/simulation.py
+++ grains/simulation.py
@@ -147,3 +147,64 @@ def hallpetch_plot(sigma_y, d, units=("MPa", "mm")):
     ax.set_title("Effect of the grain diameter on the yield strength")
     ax.legend(hp, ["Hall-Petch"])
     return fig
+
+
+def change_domain(image, left, right, bottom, top, padding_value=np.nan):
+    """Extends or crops the domain an image fills.
+
+    The original image is extended, cropped or left unchanged in the left, right, bottom and top
+    directions by padding the corresponding 2D or 3D array with a given value or removing
+    existing elements. Non-integer image length is avoided by rounding up. This choice prevents
+    ending up with an empty image during cropping or no added pixel during extension.
+
+    Parameters
+    ----------
+    image : ndarray
+        2D or 3D numpy array representing the original image.
+    left, right : float
+        When positive, the domain is extended, when negative, the domain is cropped by the given
+        value relative to the image width.
+    bottom, top : float
+        When positive, the domain is extended, when negative, the domain is cropped by the given
+        value relative to the image height.
+    padding_value : float, optional
+        Value for the added pixels. The default is numpy.nan.
+
+    Returns
+    -------
+    changed_image : ndarray
+        2D or 3D numpy array representing the modified domain.
+
+    Examples
+    --------
+    Crop an image at the top, extended it at the bottom and on the left,
+    and leave it unchanged on the right. Note the rounding for non-integer pixels.
+
+    >>> import numpy as np
+    >>> image = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]])
+    >>> modified = change_domain(image, 0.5, 0, 1/3, -1/3, 0)
+    >>> image  # no in-place modification, the original image is not overwritten
+    array([[0.1, 0.2, 0.3],
+           [0.4, 0.5, 0.6],
+           [0.7, 0.8, 0.9]])
+    >>> modified
+    array([[0. , 0. , 0.4, 0.5, 0.6],
+           [0. , 0. , 0.7, 0.8, 0.9],
+           [0. , 0. , 0. , 0. , 0. ]])
+
+    """
+    # TODO: Negative resulting image size must not be allowed. It means that left + right > width
+    #  and top + bottom>height must be fulfilled.
+    height, width = np.shape(image)
+    # number of pixels (entries in the matrix) to be added
+    left = np.ceil(width*left).astype(np.int)
+    right = np.ceil(width*right).astype(np.int)
+    bottom = np.ceil(height*bottom).astype(np.int)
+    top = np.ceil(height*top).astype(np.int)
+    # Shrink the domain (crop the matrix)
+    changed_image = image[abs(min(top, 0)):height - abs(min(bottom, 0)),
+                          abs(min(left, 0)):width - abs(min(right, 0))]
+    # Extend the domain (pad the matrix)
+    pad_width = ((max(top, 0), max(bottom, 0)), (max(left, 0), max(right, 0)))
+    changed_image = np.pad(changed_image, pad_width, constant_values=padding_value)
+    return changed_image
